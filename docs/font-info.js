c28aea1963da553a8962a39cb543c3d89658ceb8
hsjoihs | sozysozbot <jekto.vatimeliju@gmail.com>
2024-04-22 22:22:31 +0900

    Load the seven fonts

---
+++ docs/font-info.js
@@ -0,0 +1,9 @@
+const FONT_INFO = {
+  'acil_lujot': { 'uppercase': true, 'lowercase': true },
+  'cutapijot': { 'uppercase': true, 'lowercase': true },
+  'xedixel': { 'uppercase': true, 'lowercase': true },
+  'ycaxen_light': { 'uppercase': true, 'lowercase': false },
+  'ycaxen_medium': { 'uppercase': true, 'lowercase': false },
+  'syt2ten1': { 'uppercase': false, 'lowercase': true },
+  'rounded': { 'uppercase': false, 'lowercase': true },
+}
