fc7d2887cd94ccfec60217b4a946d774f4fe8171
hsjoihs | sozysozbot <jekto.vatimeliju@gmail.com>
2024-04-22 22:29:57 +0900

    Add a new property `roman_numeral_100`

--- docs/font-info.js
+++ docs/font-info.js
@@ -1,9 +1,37 @@
 const FONT_INFO = {
-  'acil_lujot': { 'uppercase': true, 'lowercase': true },
-  'cutapijot': { 'uppercase': true, 'lowercase': true },
-  'xedixel': { 'uppercase': true, 'lowercase': true },
-  'ycaxen_light': { 'uppercase': true, 'lowercase': false },
-  'ycaxen_medium': { 'uppercase': true, 'lowercase': false },
-  'syt2ten1': { 'uppercase': false, 'lowercase': true },
-  'rounded': { 'uppercase': false, 'lowercase': true },
-}
+  "acil_lujot": {
+    "uppercase": true,
+    "lowercase": true,
+    "roman_numeral_100": true
+  },
+  "cutapijot": {
+    "uppercase": true,
+    "lowercase": true,
+    "roman_numeral_100": false
+  },
+  "xedixel": {
+    "uppercase": true,
+    "lowercase": true,
+    "roman_numeral_100": true
+  },
+  "ycaxen_light": {
+    "uppercase": true,
+    "lowercase": false,
+    "roman_numeral_100": false
+  },
+  "ycaxen_medium": {
+    "uppercase": true,
+    "lowercase": false,
+    "roman_numeral_100": false
+  },
+  "syt2ten1": {
+    "uppercase": false,
+    "lowercase": true,
+    "roman_numeral_100": false
+  },
+  "rounded": {
+    "uppercase": false,
+    "lowercase": true,
+    "roman_numeral_100": true
+  }
+};
