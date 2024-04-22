c812f3639f198feba004cf9b32afded9613292b5
hsjoihs / sozysozbot <sozysozbot@gmail.com>
2024-04-23 02:23:11 +0900

    Explain the goal that I want to accomplish

--- docs/search.js
+++ docs/search.js
@@ -20,7 +20,22 @@ function get_matches() {
     });
 }
 
-function search() {
-    document.getElementById("results-section").innerHTML = JSON.stringify(get_matches(), 2, null);
+function display_result() {
+    const items = get_matches()
+    /*
+    Each item is of the following form:
+    {"item":{"source":"プロパガンダかるた","pmcp":"icco cecnutit lata pi lata cecnutit icco","direct_ja":"","ja":"国が人を守り、人が国を守る","en":""},"matched_portions":[{"match":"cecnutit","beginIndex":5,"endIndex":13},{"match":"cecnutit","beginIndex":27,"endIndex":35}]}
+
+    I would like to turn this into
+    <div class="searched-item">
+        <div class="corpus-text">icco <strong class="matched-portion">cecnutit</strong> lata pi lata cecnutit icco</div>
+        <div class="translation-ja">国が人を守り、人が国を守る</div>
+      </div>
+      <div class="searched-item">
+        <div class="corpus-text">icco cecnutit lata pi lata <strong class="matched-portion">cecnutit</strong> icco</div>
+        <div class="translation-ja">国が人を守り、人が国を守る</div>
+      </div>
+    */
+    document.getElementById("results-section").innerHTML = "/* todo */";
 }
 
