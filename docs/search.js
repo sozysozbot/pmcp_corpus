17c75e6d83202b0de5deaefa2c1145f636019182
hsjoihs / sozysozbot <sozysozbot@gmail.com>
2024-05-08 23:16:07 +0900

    Handle zero-width match

--- docs/search.js
+++ docs/search.js
@@ -14,6 +14,12 @@ function get_matches(regex_str) {
                 beginIndex: myArray.indices[0][0],
                 endIndex: myRe.lastIndex
             });
+            // zero-width match
+            if (myArray.indices[0][0] === myRe.lastIndex) {
+                // According to MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/exec
+                // > If the regex may match zero-length characters (e.g. /^/gm), increase its lastIndex manually each time to avoid being stuck in the same place.
+                myRe.lastIndex++;
+            }
         }
         return { item, matched_portions };
     });
